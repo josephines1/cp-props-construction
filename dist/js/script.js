37adbf5627c5be0eeaa09012acf4ee37a44a6c1a
josephines1 <josephine.bless777@gmail.com>
2024-07-07 21:03:09 +0700

    Add preloader

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,47 @@
+// Pre-Loader
+document.addEventListener("DOMContentLoaded", function () {
+  let percentageText = document.querySelector(".percentage");
+  let preloader = document.querySelector("#preloader");
+  let loader = document.querySelector(".loader");
+  let content = document.getElementById("content");
+
+  let totalImages = document.images.length;
+  let imagesLoaded = 0;
+
+  if (totalImages === 0) {
+    updatePreloader(100);
+    showContent();
+  } else {
+    for (let i = 0; i < totalImages; i++) {
+      let img = new Image();
+      img.src = document.images[i].src;
+      img.onload = img.onerror = function () {
+        imagesLoaded++;
+        let percentComplete = Math.floor((imagesLoaded / totalImages) * 100);
+        updatePreloader(percentComplete);
+        if (imagesLoaded === totalImages) {
+          showContent();
+        }
+      };
+    }
+  }
+
+  function updatePreloader(percent) {
+    percentageText.innerText = percent + "%";
+    loader.style.backgroundSize = percent + "% 100%";
+  }
+
+  function showContent() {
+    // Preloader disappear
+    preloader.classList.add("hide");
+
+    // Content appear
+    content.classList.add("show");
+    document.body.style.overflowY = "auto";
+  }
+});
+
+// Page Content
 document.addEventListener("DOMContentLoaded", function () {
   document.querySelector(".navbar").style.opacity = "1";
   document.querySelector("#home .fade-in-left").classList.add("active");
